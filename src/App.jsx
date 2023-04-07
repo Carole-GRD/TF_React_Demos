decf0a0f2e9d1ca1fb254806314958ca55de7ff5
Carole Gérard <gerard.carole@skynet.be>
2023-04-07 17:02:26 +0200

    schema counter

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import Counter from './containers/counter/counter';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
